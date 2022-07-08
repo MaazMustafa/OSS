1db1f261340936b1a712f9b581c098f15255d98d
Ravi Tambade <ravi.tambade@transflower.in>
2022-07-08 13:34:45 +0530

    New additions for Department collection
    Let us add one more data member to DBContext Class

--- DbORMContext.cs
+++ DbORMContext.cs
@@ -7,12 +7,14 @@ using MySql.Data.EntityFramework;
 using ORMFrameWork.Models;
 
 //This is a comment
+//ORM DB Context class need to be added with Departments DB Set As well
 namespace ORMFrameWork.DAL
 {
     [DbConfigurationType(typeof(MySqlEFConfiguration))]
     public class DbORMContext:DbContext
     {
         public DbSet<Employee> Employees { get; set; }
+        public DbSet<Department> Departments {get;set}
         public DbORMContext():base("con")
         {
 
