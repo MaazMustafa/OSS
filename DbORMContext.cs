791478438490f03befe062b0ea232fbe586fbf1e
MaazMustafa <108923279+MaazMustafa@users.noreply.github.com>
2022-07-08 13:18:45 +0530

    Add files via upload

---
+++ DbORMContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using MySql.Data.EntityFramework;
+using ORMFrameWork.Models;
+
+
+namespace ORMFrameWork.DAL
+{
+    [DbConfigurationType(typeof(MySqlEFConfiguration))]
+    public class DbORMContext:DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+        public DbORMContext():base("con")
+        {
+
+        }
+    }
+}
