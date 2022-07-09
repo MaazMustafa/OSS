52901c005305c1f5d50a1f8f12a31e00e376b386
Ravi Tambade <ravi.tambade@transflower.in>
2022-07-09 12:27:04 +0530

    Connected Data Access changes
    We would use  connection ,command, DataReader  inside

---
+++ DAL/connecteddal.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Data.MySqlClient
+using System.Data
+using BOL;
+
+namespance DAL{
+
+public class DBManager{
+
+public static string conString=@"";
+
+
+}
+}
