006e0822baf7ee357179bd5c7f8f6cfdae167721
Ravi Tambade <ravi.tambade@transflower.in>
2022-07-09 12:49:31 +0530

    dataset and customer List retrival

--- DAL/dbmanager.cs
+++ DAL/dbmanager.cs
@@ -9,12 +9,22 @@ public class DBManager{
   public static string conString="server=localhost, database='actsdb'";
   public static List<Customer> GetAll(){
   
+    List<Customer> customers=new List<Customer>();
     MySqlConnection con=new MySqlConnection(conString);
     string query="SELECT * from customers";
     MySqlCommand cmd=new MySqlCommand(query, con);
     try{
       MySqlDataAdapter da=new MySqlDataAdapter(cmd);
       da.Fill(ds);
+      DataTable dt=ds.Tables[0];
+      DataRowCollection rows=dt.Rows;
+      foreach(DataRow row in rows){
+        Customer cst=new Customer();
+        cst.Id=int.Parse(row["Id"].ToString());
+        cst.FirstName=row["FirstName"].ToString();
+
+        customers.Add(cst);
+      }
    
     }
     catch(Exception ee)
